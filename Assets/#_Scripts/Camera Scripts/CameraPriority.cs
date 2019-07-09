afbf3b6dc348ff70ae3bf844847a3272e18267e4
rosati16 <50802836+rosati16@users.noreply.github.com>
2019-07-09 18:13:54 -0400

    camera switching with triggers

---
+++ Assets/#_Scripts/Camera Scripts/CameraPriority.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraPriority : MonoBehaviour
+{
+
+    //[SerializeField]
+    //public Camera MainCamera;
+
+    [SerializeField]
+    public CinemachineVirtualCamera CameraToSwitchTo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CameraToSwitchTo.GetComponent<CinemachineVirtualCamera>().Priority = 100;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraToSwitchTo.GetComponent<CinemachineVirtualCamera>().Priority = 0;
+        }
+    }
+
+}
