a02e4d64bc28328b06e6755cbd2f2713aff48e72
PinHeadGeneric <51333494+PinHeadGeneric@users.noreply.github.com>
2019-07-30 20:43:20 -0400

    More level fleshing
    Chunck and chris

--- Assets/#_Scripts/Throwing.cs
+++ Assets/#_Scripts/Throwing.cs
@@ -138,6 +138,7 @@ public class Throwing : MonoBehaviour
     {
         if (heldObj && heldObj.tag == "Pushable")
         {
+            animator.SetTrigger("StopGrab");
             DropPushable();
         }
     }
@@ -213,7 +214,7 @@ public class Throwing : MonoBehaviour
         heldObj.GetComponent<Rigidbody>().interpolation = objectDefInterpolation;
         heldObj.GetComponent<Collider>().material = null;
         Destroy(joint);
-        animator.SetTrigger("StopGrab");
+
         playerMove.rotateSpeed = defRotateSpeed;
         heldObj = null;
         timeOfThrow = Time.time;
